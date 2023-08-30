3ca6217ce6b584be288d3d5bc239ed2781bf3095
Maria Gornashevich <lightbrown9874@gmail.com>
2023-08-31 00:13:42 +0300

    feat: add error boundary

---
+++ src/components/error-boundary/errorBoundary.tsx
@@ -0,0 +1,15 @@
+import { ErrorBoundary } from "react-error-boundary";
+
+interface Props {
+  children: JSX.Element | null;
+}
+
+const ErrorBoundaryWithMessage = ({ children }: Props) => {
+  return (
+    <ErrorBoundary fallback={<div>Something went wrong...</div>}>
+      {children}
+    </ErrorBoundary>
+  );
+};
+
+export { ErrorBoundaryWithMessage };
