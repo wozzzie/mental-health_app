e00830616f829e73885a5e8049cf6e17a270aaad
Maria Gornashevich <lightbrown9874@gmail.com>
2023-09-02 20:08:15 +0300

    feat: add auth

---
+++ src/pages/auth/sign-up.tsx
@@ -0,0 +1,49 @@
+import nookies from "nookies";
+import { GetServerSidePropsContext } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+import { registerWithEmailAndPassword } from "../../firebase/firebaseClient";
+import { firebaseAdmin } from "../../firebase/firebaseAdmin";
+import ROUTES from "../../constants/routes";
+import AuthView from "../../components/auth/authView";
+import { ErrorBoundaryWithMessage } from "../../components/error-boundary/errorBoundary";
+
+export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  try {
+    const cookies = nookies.get(ctx);
+    const token = await firebaseAdmin.auth().verifyIdToken(cookies.token);
+    const { uid } = token;
+
+    return {
+      redirect: {
+        permanent: false,
+        destination: ROUTES.APP,
+      },
+      props: {
+        uid,
+      },
+    };
+  } catch (err) {
+    const locale = ctx.locale || "en";
+
+    return {
+      props: {
+        ...(await serverSideTranslations(locale, ["common"])),
+      },
+    };
+  }
+};
+
+const SignUp = () => {
+  const onSignUp = async (email: string, password: string) => {
+    return await registerWithEmailAndPassword(email, password);
+  };
+
+  return (
+    <ErrorBoundaryWithMessage>
+      <AuthView authCallback={onSignUp} page="SIGN_UP" />
+    </ErrorBoundaryWithMessage>
+  );
+};
+
+export default SignUp;
