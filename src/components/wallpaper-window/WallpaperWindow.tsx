b0aa543eb5bd644bac2605cbcaf4c1f88cce3877
Kiryl Paulouski <kirr2156@gmail.com>
2023-12-09 14:20:25 +0100

    feat: wallpaper-window transition add

--- src/components/wallpaper-window/WallpaperWindow.tsx
+++ src/components/wallpaper-window/WallpaperWindow.tsx
@@ -4,11 +4,16 @@ import React from "react";
 
 type Props = {
   children?: React.ReactNode[] | React.ReactNode;
+  transitionState: string;
 };
 
-const WallpaperWindow: React.FC<Props> = ({ children }) => {
+const WallpaperWindow: React.FC<Props> = ({ children, transitionState: s }) => {
+  const windowStyles =
+    styles["wallpaper-window"] +
+    " " +
+    (s === "exited" || s === "exiting" ? styles["wallpaper-window_exit"] : "");
   return (
-    <div className={styles["wallpaper-window"]}>
+    <div className={windowStyles}>
       <WidgetWrapper className={styles["widget-wrapper"]}>
         {children}
       </WidgetWrapper>
