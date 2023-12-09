bbf2e729c5a7514fca7712b27868974459c38057
Kiryl Paulouski <kirr2156@gmail.com>
2023-12-09 14:20:59 +0100

    refactor: make dynamic button render in widgetbar

--- src/components/widgetbar/Widgetbar.tsx
+++ src/components/widgetbar/Widgetbar.tsx
@@ -3,22 +3,75 @@ import Image from "next/image";
 import styles from "./style.module.scss";
 import WidgetWrapper from "../widget-wrapper/WidgetWrapper";
 import { useDispatch } from "react-redux";
-import { openWidget, toggleWidget, toggleWallpaperWindow } from "../screen/ScreenSlice";
-import { useState, useMemo } from "react"
+import {
+  openWidget,
+  toggleWidget,
+  toggleWallpaperWindow,
+} from "../screen/ScreenSlice";
+import { useState, useMemo, FC, useRef, useEffect } from "react";
 
+type WidgetButton = {
+  img: {
+    src: string;
+    alt: string;
+  };
+  action: Function;
+  key: any;
+  active: boolean;
+};
 
-const Widgetbar = () => {
+type Props = {
+  buttons: WidgetButton[];
+};
 
+const Widgetbar: FC<Props> = ({ buttons }) => {
   const dispatch = useDispatch();
-
   const [wrapperActive, setWrapperActive] = useState<boolean>(true);
 
-  const wrapperClasses =  useMemo(() => styles["widget"] + (wrapperActive ? " " + styles["widget_active"] : ""), [wrapperActive]);
+  const [overflow, setOverflow] = useState<boolean>(false);
 
+  const timeout = useRef<ReturnType<typeof setTimeout> | null>(null);
+
+  const handleButtonMouseOver = () => {
+    if (timeout.current !== null) clearTimeout(timeout.current);
+    if (wrapperActive) setOverflow(true);
+  };
+
+  const handleButtonMouseOut = () => {
+    timeout.current = setTimeout(() => {
+      setOverflow(false);
+      timeout.current = null;
+      console.log("time");
+    }, 500);
+  };
+
+  useEffect(() => {
+    console.log("overflow ", overflow);
+  }, [overflow]);
+
+  const wrapperClasses = useMemo(
+    () =>
+      styles["widget"] +
+      (wrapperActive ? " " + styles["widget_active"] : "") +
+      (overflow ? " " + styles["widget_visible"] : ""),
+    [wrapperActive, overflow]
+  );
+
+  const handleToggleWrapperActive = () => {
+    if (wrapperActive) {
+      setOverflow(false);
+      setWrapperActive(false);
+    } else {
+      setWrapperActive(true);
+    }
+  };
 
   return (
     <WidgetWrapper className={wrapperClasses}>
-      <div className={styles["widget__menu"]} onClick={()=>setWrapperActive(s => !s)}>
+      <div
+        className={styles["widget__menu"]}
+        onClick={handleToggleWrapperActive}
+      >
         <Image
           src="/burger-menu.svg"
           width={18}
@@ -27,7 +80,11 @@ const Widgetbar = () => {
         />
       </div>
       <div className={styles["widget__block_main"]}>
-        <div className={styles["widget__gallery"]} onClick={()=>dispatch(toggleWallpaperWindow())}>
+        <div className={styles["widget__block_main_in"]}>
+          {/* <div
+          className={styles["widget__gallery"]}
+          onClick={() => dispatch(toggleWallpaperWindow())}
+        >
           <Image
             src="/wallpaper.svg"
             width={24}
@@ -35,16 +92,25 @@ const Widgetbar = () => {
             alt="Backgrounds gallery"
           />
         </div>
-        <div className={styles["widget__music"]} onClick={()=>dispatch(toggleWidget("music"))}>
+        <div
+          className={styles["widget__music"]}
+          onClick={() => dispatch(toggleWidget("music"))}
+        >
           <Image src="/music.svg" width={24} height={24} alt="Play" />
         </div>
         <div className={styles["widget__breath"]}>
           <Image src="/breath.svg" width={24} height={24} alt="Breath" />
         </div>
-        <div className={styles["widget__quotes"]} onClick={()=>dispatch(toggleWidget("quote"))}>
+        <div
+          className={styles["widget__quotes"]}
+          onClick={() => dispatch(toggleWidget("quote"))}
+        >
           <Image src="/quotes.svg" width={24} height={24} alt="Quotes" />
         </div>
-        <div className={styles["widget__meditation"]} onClick={()=>dispatch(toggleWidget("meditation"))}>
+        <div
+          className={styles["widget__meditation"]}
+          onClick={() => dispatch(toggleWidget("meditation"))}
+        >
           <Image
             src="/meditation.svg"
             width={24}
@@ -52,12 +118,18 @@ const Widgetbar = () => {
             alt="Meditation"
           />
         </div>
-        <div className={styles["widget__news"]} onClick={()=>dispatch(toggleWidget("news"))}>
+        <div
+          className={styles["widget__news"]}
+          onClick={() => dispatch(toggleWidget("news"))}
+        >
           <Image src="/news.svg" width={24} height={24} alt="News" />
         </div>
       </div>
-      <div className={styles["widget__block_additional"]} >
-        <div className={styles["widget__gif"]} onClick={()=>dispatch(toggleWidget("gif"))}>
+      <div className={styles["widget__block_additional"]}>
+        <div
+          className={styles["widget__gif"]}
+          onClick={() => dispatch(toggleWidget("gif"))}
+        >
           <Image
             src="/gif-widget.svg"
             width={24}
@@ -67,8 +139,31 @@ const Widgetbar = () => {
         </div>
         <div className={styles["widget__settings"]}>
           <Image src="/settings.svg" width={19.49} height={20} alt="Settings" />
+        </div> */}
+          {buttons.map((i: WidgetButton) => (
+            <div
+              key={i.key}
+              onClick={i.action}
+              className={
+                styles["widget-button"] +
+                (i.active ? " " + styles["widget-button_active"] : "")
+              }
+              onMouseOver={handleButtonMouseOver}
+              onMouseOut={handleButtonMouseOut}
+            >
+              <Image
+                src={i.img.src}
+                alt={i.img.alt}
+                width={24}
+                height={24}
+                title={i.img.alt}
+              />
+              <div className={styles["widget-button__title"]}>{i.img.alt}</div>
+            </div>
+          ))}
         </div>
       </div>
+      <div></div>
     </WidgetWrapper>
   );
 };
