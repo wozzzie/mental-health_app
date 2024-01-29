5f6b495175104a1d0ea0fbd2440601ba4b163e8b
Kiryl Paulouski <kirr2156@gmail.com>
2024-01-29 22:11:04 +0100

    fix: change prop types for app button

--- src/components/app-button/AppButton.tsx
+++ src/components/app-button/AppButton.tsx
@@ -2,14 +2,16 @@ import { FC, ReactNode } from "react";
 import styles from "./style.module.scss";
 
 interface Props {
-  onClick: () => void;
+  onClick?: () => void;
   children: ReactNode;
   className?: string;
+  type: "button" | "submit" | "reset" | undefined;
 }
 
-const AppButton: FC<Props> = ({ onClick, children, className }) => {
+const AppButton: FC<Props> = ({ onClick, children, className, type }) => {
   return (
     <button
+      type={type}
       onClick={onClick}
       className={styles["app-btn"] + (className ? " " + className : "")}
     >
