9d813986c203922d7db350caa4c368e86f7f912c
Kiryl Paulouski <kirr2156@gmail.com>
2024-02-23 17:55:22 +0100

    feat: add gif widget

--- src/components/gif-widget/GifWidget.tsx
+++ src/components/gif-widget/GifWidget.tsx
@@ -1,11 +1,39 @@
-import { FC } from "react";
+import { FC, useLayoutEffect, useState } from "react";
 import styles from "./style.module.scss";
 import useKittenGifs from "@/hooks/kittenGifs.hook";
+import { useGetGifQuery } from "@/apis/gif.api";
+import Image from "next/image";
+import WidgetWrapper from "../widget-wrapper/WidgetWrapper";
+import useGif from "@/hooks/gif.hook";
 
 const GifWidget: FC<{}> = () => {
-  const { gif, status, updateGif } = useKittenGifs();
+  const { data, isFetching, isError, refetch } = useGif();
 
-  return <></>;
+  const [gifIndex, setGifIndex] = useState<number>(0);
+
+  useLayoutEffect(() => {
+    setGifIndex(Math.floor(Math.random() * 50));
+  }, []);
+
+  return (
+    <WidgetWrapper className={styles["gif"]}>
+      {isFetching ? (
+        "loading..."
+      ) : isError ? (
+        "error!"
+      ) : data ? (
+        <Image
+          className={styles["gif__image"]}
+          src={data[gifIndex].url as string}
+          alt={"Another cat gif"}
+          width={200}
+          height={200}
+        />
+      ) : (
+        ""
+      )}
+    </WidgetWrapper>
+  );
 };
 
 export default GifWidget;
