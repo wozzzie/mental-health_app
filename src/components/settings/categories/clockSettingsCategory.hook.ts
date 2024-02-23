3f38f024e1ad058db2aed05a0ee4553a04f3bf25
Kiryl Paulouski <kirr2156@gmail.com>
2024-02-23 17:59:36 +0100

    feat: add clock settings category

--- src/components/settings/categories/clockSettingsCategory.hook.ts
+++ src/components/settings/categories/clockSettingsCategory.hook.ts
@@ -1,10 +1,34 @@
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { settingsGroup } from "../settingsProvider";
+import { useGetSettingsQuery, useSetSettingsMutation } from "@/apis/clock.api";
+import { useAuth } from "@/components/auth/authProvider";
 
 const useClockSettingsCategory = () => {
   const [use24HourFormat, setUse24HourFormat] = useState<boolean>(false);
 
   const [showSeconds, setShowSeconds] = useState<boolean>(false);
+
+  const uid = useAuth()?.user?.uid;
+
+  const { data, isSuccess } = useGetSettingsQuery(uid || "");
+
+  const [updateSettings, res] = useSetSettingsMutation();
+
+  useEffect(() => {
+    if (data && isSuccess) {
+      setUse24HourFormat(data.use24HourFormat);
+      setShowSeconds(data.showSeconds);
+    }
+  }, [isSuccess]);
+
+  useEffect(() => {
+    if (uid)
+      updateSettings({
+        use24HourFormat,
+        showSeconds,
+        uid,
+      });
+  }, [use24HourFormat, showSeconds]);
 
   const changeTimeFormat = (format: boolean) => {
     setUse24HourFormat(format);
