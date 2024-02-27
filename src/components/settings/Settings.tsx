6a5aa066a83e1731e336336f56ca9e753e9a7c4c
Kiryl Paulouski <kirr2156@gmail.com>
2024-02-27 17:40:09 +0100

    fix: change settings render scheme

--- src/components/settings/Settings.tsx
+++ src/components/settings/Settings.tsx
@@ -21,6 +21,9 @@ import {
   TransitionStatus,
 } from "react-transition-group";
 import SettingsForm from "./SettingsForm";
+import { createPortal } from "react-dom";
+import { useDispatch } from "react-redux";
+import { closeSettingsWindow } from "../screen/ScreenSlice";
 
 type Props = {
   transitionState: TransitionStatus;
@@ -35,113 +38,137 @@ const Settings: FC<Props> = ({ transitionState }) => {
 
   const { user } = useAuth();
 
-  return (
-    <WidgetWrapper
-      className={
-        styles["settings__wrapper"] +
-        (transitionState === "entering" || transitionState === "entered"
-          ? " " + styles["settings__wrapper_visible"]
-          : "")
+  const dispatch = useDispatch();
+
+  return createPortal(
+    <div
+      className={styles["settings__bounds"]}
+      style={{
+        opacity:
+          transitionState === "entered" || transitionState === "entering"
+            ? 1
+            : 0,
+        zIndex:
+          transitionState === "entered" ||
+          transitionState === "entering" ||
+          transitionState === "exiting"
+            ? 200
+            : -1,
+      }}
+      onClick={(e) =>
+        e.currentTarget === e.target ? dispatch(closeSettingsWindow()) : null
       }
     >
-      <h2 className={styles["settings__header"]}>{t("settings.settings")}</h2>
-      <div className={styles["settings"]}>
-        <div className={styles["settings__groups"]}>
-          {settings.groups.map((group, i) => (
-            <div
-              key={i}
-              className={
-                styles["settings__group"] +
-                (i === chosenGroup
-                  ? " " + styles["settings__group_active"]
-                  : "")
-              }
-              onClick={() => setChosenGroup(i)}
-            >
-              {t("settings." + group.name + ".group-name")}
-            </div>
-          ))}
-        </div>
-        <div className={styles["settings__divider"]}></div>
-        <SwitchTransition>
-          <Transition key={chosenGroup} timeout={300} mountOnEnter>
-            {(s) => (
+      <WidgetWrapper
+        className={
+          styles["settings__wrapper"] +
+          (transitionState === "entering" || transitionState === "entered"
+            ? " " + styles["settings__wrapper_visible"]
+            : "")
+        }
+      >
+        <h2 className={styles["settings__header"]}>{t("settings.settings")}</h2>
+        <div className={styles["settings"]}>
+          <div className={styles["settings__groups"]}>
+            {settings.groups.map((group, i) => (
               <div
-                className={styles["settings__chosen"]}
-                style={{
-                  transition: "300ms all",
-                  opacity: s === "entered" || s === "entering" ? 1 : 0,
-                }}
+                key={i}
+                className={
+                  styles["settings__group"] +
+                  (i === chosenGroup
+                    ? " " + styles["settings__group_active"]
+                    : "")
+                }
+                onClick={() => setChosenGroup(i)}
               >
-                {(settings.groups[chosenGroup] as settingsGroup).settings.map(
-                  (item, i) => (
-                    <div
-                      className={styles["settings__element"]}
-                      key={i + " " + chosenGroup}
-                    >
-                      {item.type !== "controlled" && (
-                        <div className={styles["settings__description"]}>
-                          {t(
-                            "settings." +
-                              settings.groups[chosenGroup].name +
-                              "." +
-                              item.name
-                          )}
-                        </div>
-                      )}
-                      {item.type === "switch" ? (
-                        <SettingsSwitch
-                          isActive={item.value}
-                          onChange={(isActive: boolean) => {
-                            // settings.change((s) => {
-                            //   const tmp = { ...s };
-                            //   tmp.groups[chosenGroup].settings[i] = {
-                            //     ...tmp.groups[chosenGroup].settings[i],
-                            //     value: isActive,
-                            //   } as switchSetting;
-                            //   return tmp;
-                            // });
-                            item.callback(isActive);
-                          }}
-                        />
-                      ) : item.type === "select-form" ? (
-                        <div className={styles["settings__select"]}>
-                          <Select
-                            options={item.valueVariants.filter(
-                              (i) => i !== item.selectedValue
+                {t("settings." + group.name + ".group-name")}
+              </div>
+            ))}
+          </div>
+          <div className={styles["settings__divider"]}></div>
+          <SwitchTransition>
+            <Transition key={chosenGroup} timeout={300} mountOnEnter>
+              {(s) => (
+                <div
+                  className={styles["settings__chosen"]}
+                  style={{
+                    transition: "300ms all",
+                    opacity: s === "entered" || s === "entering" ? 1 : 0,
+                  }}
+                >
+                  {(settings.groups[chosenGroup] as settingsGroup).settings.map(
+                    (item, i) => (
+                      <div
+                        className={styles["settings__element"]}
+                        key={i + " " + chosenGroup}
+                      >
+                        {item.type !== "controlled" && (
+                          <div className={styles["settings__description"]}>
+                            {t(
+                              "settings." +
+                                settings.groups[chosenGroup].name +
+                                "." +
+                                item.name
                             )}
-                            inputValue={
-                              item.selectedValue || t("settings.select-default")
-                            }
-                            onChange={(option: string) => {
+                          </div>
+                        )}
+                        {item.type === "switch" ? (
+                          <SettingsSwitch
+                            isActive={item.value}
+                            onChange={(isActive: boolean) => {
                               // settings.change((s) => {
                               //   const tmp = { ...s };
                               //   tmp.groups[chosenGroup].settings[i] = {
                               //     ...tmp.groups[chosenGroup].settings[i],
-                              //     selectedValue: option,
-                              //   } as selectFormSetting;
+                              //     value: isActive,
+                              //   } as switchSetting;
                               //   return tmp;
                               // });
-                              item.callback(option);
+                              item.callback(isActive);
                             }}
                           />
-                        </div>
-                      ) : item.type === "form" ? (
-                        <SettingsForm formSetting={item} />
-                      ) : item.type === "controlled" ? (
-                        item.component
-                      ) : (
-                        ""
-                      )}
-                    </div>
-                  )
-                )}
-              </div>
-            )}
-          </Transition>
-        </SwitchTransition>
-      </div>
-    </WidgetWrapper>
+                        ) : item.type === "select-form" ? (
+                          <div className={styles["settings__select"]}>
+                            <Select
+                              options={item.valueVariants.filter(
+                                (i) => i !== item.selectedValue
+                              )}
+                              inputValue={
+                                item.selectedValue ||
+                                t("settings.select-default")
+                              }
+                              onChange={(option: string) => {
+                                // settings.change((s) => {
+                                //   const tmp = { ...s };
+                                //   tmp.groups[chosenGroup].settings[i] = {
+                                //     ...tmp.groups[chosenGroup].settings[i],
+                                //     selectedValue: option,
+                                //   } as selectFormSetting;
+                                //   return tmp;
+                                // });
+                                item.callback(option);
+                              }}
+                            />
+                          </div>
+                        ) : item.type === "form" ? (
+                          <SettingsForm formSetting={item} />
+                        ) : item.type === "controlled" ? (
+                          item.component
+                        ) : (
+                          ""
+                        )}
+                      </div>
+                    )
+                  )}
+                </div>
+              )}
+            </Transition>
+          </SwitchTransition>
+        </div>
+      </WidgetWrapper>
+    </div>,
+
+    document.body
   );
 };
 
